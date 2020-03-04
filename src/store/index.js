52987247cca8d1a8452f89144bdb9bb5e562223d
Tim Dohmen <dohmentim@yahoo.com>
2020-03-03 17:59:04 -0700

    notification fireside

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,57 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { Store } from 'vuex'
+import Axios from "axios"
 
 Vue.use(Vuex)
 
+let gifApi = Axios.create({
+  baseURL: "https://api.giphy.com/v1/gifs/trending?api_key=GKVvQKzGg46Nc9BfWt7GyJ5GJJlfK49i",
+  timeout: 3000
+})
+
 export default new Vuex.Store({
   state: {
+    gifs: [],
+    myGifs: []
   },
   mutations: {
+    setGifs(state, gifs) {
+      state.gifs = gifs
+    },
+    addGif(state, gif) {
+      state.myGifs.push(gif)
+    },
+    removeGif(state, gif) {
+      let index = state.myGifs.findIndex(g => g.id == gif)
+      state.myGifs.splice(index, 1)
+    }
   },
   actions: {
+    async getGifs({ commit, dispatch }) {
+      try {
+        let res = await gifApi.get("")
+        commit("setGifs", res.data.data)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    addGif({ commit, dispatch }, gif) {
+      try {
+        commit("addGif", gif)
+      } catch (error) {
+        console.error(error)
+
+      }
+    },
+    removeGif({ commit, dispatch }, gifId) {
+      try {
+        commit("removeGif", gifId)
+      } catch (error) {
+        console.error(error)
+
+      }
+    }
+
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Gifmon</h3>
+    <div v-for="gif in gifs" :key="gif.id">
+      <img :src="gif.embed_url" alt="unloadedGif" />
+      <p>{{gif.title}}</p>Ello
+      <button @click="addGif(gif)" class="btn btn-primary">Add Gif</button>
+    </div>
+    <div v-for="gif in myGifs" :key="gif.id">
+      <img :src="gif.embed_url" alt="unloadedGif" />
+      <p>{{gif.title}}</p>Ello
+      <button @click="removeGif(gif.id)" class="btn btn-danger">Remove Gif</button>
+    </div>
+
+    <footer>
+      Do the thing input some data
+      <button class="btn btn-success" @click="dataInput()">Form Input</button>
+    </footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import NotificationService from "../NotificationService.js";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.$store.dispatch("getGifs");
+  },
+  computed: {
+    gifs() {
+      return this.$store.state.gifs;
+    },
+    myGifs() {
+      return this.$store.state.myGifs;
+    }
+  },
+  methods: {
+    addGif(gif) {
+      this.$store.dispatch("addGif", gif);
+      NotificationService.toast("custom handler");
+    },
+    async removeGif(gifId) {
+      if (
+        await NotificationService.confirmAction(
+          "This is a test to remove things"
+        )
+      ) {
+        this.$store.dispatch("removeGif", gifId);
+      }
+    },
+    async dataInput() {
+      let data = await NotificationService.inputData();
+      console.log(data);
+    },
+
+    async logout() {
+      if (
+        await NotificationService.confirmAction(
+          "Are you sure you want to log out?"
+        )
+      ) {
+        this.$store.dispatch("logout");
+      }
+    }
   }
-}
+};
 </script>
+
+
+<style>
+img {
+  min-height: 100px;
+  min-width: 100px;
+}
+</style>
